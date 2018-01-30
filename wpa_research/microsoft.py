744cfac3292888e245ed47384a4891f44a39980e
Arseniy Sharoglazov <mohemiv@gmail.com>
2018-01-31 02:45:08 +0300

    Added PRF+ function

--- wpa_research/microsoft.py
+++ wpa_research/microsoft.py
@@ -8,3 +8,30 @@ def nt_hash(str, binary=False):
 
 def sha1(str):
     return hashlib.new("sha1", str).digest();
+
+# RFC 4306 2.13
+def prf_peap_plus (secret, label, seed, length, peap_version = 0):
+    if length >= 256:
+        raise Exception("length value must be no greater than 255");
+
+    seed = label + seed;
+
+    res = "";
+    prev = ""
+    i = 0;
+
+    if peap_version == 0:
+        prefix = "";
+        postfix = "\x00\x00"
+    elif peap_version == 1:
+        prefix = pack('B', length);
+        postfix = "";
+    else:
+      raise Exception("peap_version value must be 0 or 1");
+
+    while len(res) < length:
+        i += 1;
+        prev = hmac.new(secret, prev + seed + prefix + pack('B', i) + postfix, hashlib.sha1).digest();
+        res += prev;
+
+    return res[:length];
